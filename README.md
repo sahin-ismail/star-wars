4a2dc87d859f298b58c5f0bf8b3572b160a2d92e
sahin-ismail <sahinismail0618@gmail.com>
2024-06-23 01:53:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Star Wars Application
+It is a sample NodeJs application.
+
+![](header.png)
+
+## Installation and Run Server
+
+OS X & Linux:
+
+```sh
+npm install
+```
+```sh
+npm start
+```
---
+++ app.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var request = require('request');
+var fs = require('fs');
+app.set("view engine","ejs");
+
+app.use((req,res,next)=>{
+   var zaman = new Date().toString();
+
+   var log = zaman +" "+ req.url;
+   console.log(log);
+   fs.appendFile('server.log',log + '\n',(err)=>{
+      if(err){
+        console.log(err);
+      }
+   });
+   next();
+});
+
+/*
+app.use((req,res,next)=>{
+   res.render('calisma');
+});
+*/
+
+//ÜZERİNİ YORUMA ALDIM ÇÜNKÜ HEPSİNE BAKIMDA YAZIYOR
+app.get("/",(req,res) => {
+    res.render('arama');
+});
+
+app.get('/sonuc',(req,res)=>{
+    var sorgu = req.query.sorgu;
+    var url = "https://swapi.co/api/people/?search=" + sorgu;
+   request(url,(error,response,body)=>{
+     if(!error && response.statusCode == 200){
+         var veri = JSON.parse(body);
+         res.render('sonuc',{
+             veri: veri
+         });
+     }
+   });
+});
+
+app.listen(process.env.PORT);
